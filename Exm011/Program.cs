68406d1a7910277e4a7e53371bb7a7ab9aa53145
allajka <allajka94@gmail.com>
2022-08-22 22:51:13 +0300

    Exm011-013

---
+++ Exm011/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Задача 100: 10000<N<30000. нужно получить разбиение всех чисел от 2 до N на простые множители
+Другими словами, n > 1 – простое, если при его делении на любое число кроме 1 и n есть остаток.
+
+*/
+bool IsSimple(int number)
+{
+    if (number <= 1) return false;
+    for (int j = 2; j <= Math.Sqrt(number); j++)
+    {
+        if (number % j == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void CreateSimple(int lastNumber, int[] arr)
+{
+    int count = 1;
+    for (int i = 3; i < lastNumber; i += 2)
+    {
+        if (IsSimple(i))
+        {
+            arr[count] = i;
+            count++;
+        }
+    }
+}
+
+void ReturnSimpleList(int number, int[] arraySimpleNumber)
+{
+    string result = $"{number} = 1 ";
+    int i = 0;
+    while (i < number)
+    {
+        if (number % arraySimpleNumber[i] == 0)  // 4% 0 ==0 || 2%
+        {
+            number /= arraySimpleNumber[i]; // number = 2
+            result = result + $"* {arraySimpleNumber[i]} "; // result * 2
+            i = 0;
+        }
+        else i++;
+    }
+    Console.WriteLine($"{result}");
+}
+
+DateTime start = DateTime.Now;
+int lastNumber = 30000; // до какого числа смотрим
+int[] arraySimpleNumber = new int[lastNumber / 8];
+arraySimpleNumber[0] = 2;
+CreateSimple(lastNumber, arraySimpleNumber);
+
+for (int i = 2; i < lastNumber; i++)
+{
+    ReturnSimpleList(i, arraySimpleNumber);
+}
+DateTime end = DateTime.Now;
+Console.WriteLine($"end - start: {end - start}");
---
+++ Exm013/Program.cs
@@ -0,0 +1,64 @@
+﻿/* 1.Показать двумерный массив размером m×n заполненный целыми числами
+2. Показать двумерный массив размером m×n заполненный вещественными числами
+3. В двумерном массиве n×k заменить четные элементы на противоположные ( 4 , -4)
+4. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+5. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+6. В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет
+7. В матрице чисел найти сумму элементов главной диагонали*/
+
+// В двумерном массиве n×k заменить четные элементы на противоположные
+
+int[,] array = new int[3, 3];
+void FillArray(int[,] array)
+{
+    int rows = array.GetLength(0), cols = array.GetLength(1);
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            array[row, col] = new Random().Next(-10, 10);
+        }
+    }
+}
+
+void change (int[,] array)
+{
+    int rows = array.GetLength(0), cols = array.GetLength(1);
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            if (array[row, col] % 2 == 0)
+            {
+                array[row, col] = array[row, col];
+                if (array[row, col] > 0)
+                {
+                    array[row, col] = -array[row, col];
+                }
+                if (array[row, col] < 0)
+                {
+                    array[row, col] = array[row, col];
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0), cols = array.GetLength(1);
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            Console.Write($"{array[row, col],3}");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
+change(array);
+Console.WriteLine();
+
+PrintArray(array);
